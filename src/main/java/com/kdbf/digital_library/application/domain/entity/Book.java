39972de07e84ae017bfa7ca4d8fb5a61ebba2b2f
bibi <kevindaniel707070@gmail.com>
2026-01-23 09:10:42 -0400

    Create book entity

---
+++ src/main/java/com/kdbf/digital_library/application/domain/entity/Book.java
@@ -0,0 +1,61 @@
+package com.kdbf.digital_library.application.domain.entity;
+
+import java.util.Set;
+import java.util.UUID;
+
+import lombok.Getter;
+
+@Getter
+public class Book {
+
+  UUID id;
+  String identifier; // An alternative identifier to ISBN
+  Set<String> authors; // a book can be done by many people
+  String title;
+  String description;
+  String publisher;
+  int pageCount;
+
+  public Book(String identifier, Set<String> authors, String title, String description, String publisher,
+      int pageCount) {
+    this.id = UUID.randomUUID();
+    this.identifier = identifier;
+    this.authors = authors;
+    this.title = title;
+    this.description = description;
+    this.publisher = publisher;
+    this.pageCount = pageCount;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((id == null) ? 0 : id.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    Book other = (Book) obj;
+    if (id == null) {
+      if (other.id != null)
+        return false;
+    } else if (!id.equals(other.id))
+      return false;
+    return true;
+  }
+
+  @Override
+  public String toString() {
+    return "Book [id=" + id + ", identifier=" + identifier + ", authors=" + authors + ", title=" + title
+        + ", description=" + description + ", publisher=" + publisher + ", pageCount=" + pageCount + "]";
+  }
+
+}
