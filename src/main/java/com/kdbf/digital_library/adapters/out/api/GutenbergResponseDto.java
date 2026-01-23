8113ee46bae390fc710732678e90839163bea046
bibi <kevindaniel707070@gmail.com>
2026-01-23 12:33:52 -0400

    Create DTO for gutenberg api

---
+++ src/main/java/com/kdbf/digital_library/adapters/out/api/GutenbergResponseDto.java
@@ -0,0 +1,12 @@
+package com.kdbf.digital_library.adapters.out.api;
+
+import java.util.List;
+import java.util.Set;
+
+public record GutenbergResponseDto(Long count, List<GutenbergBookDto> result) {
+  public record GutenbergBookDto(int id, String title, Set<GutenbergPersonDto> authors) {
+  }
+
+  public record GutenbergPersonDto(String name) {
+  }
+}
