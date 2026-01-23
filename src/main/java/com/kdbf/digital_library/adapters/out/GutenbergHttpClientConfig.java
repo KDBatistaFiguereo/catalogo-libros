2d5ebc5e24f3ad506f7dbcfcf2b01134ece58205
bibi <kevindaniel707070@gmail.com>
2026-01-22 20:43:03 -0400

    Create HttpClient bean

---
+++ src/main/java/com/kdbf/digital_library/adapters/out/GutenbergHttpClientConfig.java
@@ -0,0 +1,18 @@
+package com.kdbf.digital_library.adapters.out;
+
+import java.net.http.HttpClient;
+import java.time.Duration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GutenbergHttpClientConfig {
+
+  @Bean
+  public HttpClient gutenbergHttpClient() {
+    return HttpClient.newBuilder()
+        .connectTimeout(Duration.ofSeconds(20))
+        .build();
+  }
+}
